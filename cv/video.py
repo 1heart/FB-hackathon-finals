168dd0c0d17ff6af69c931bab3450c636636f4c9
1heart <yixin1996@gmail.com>
2015-11-19 16:46:46 -0800

    youtube download + open

--- cv/video.py
+++ cv/video.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
-import youtube_dl
+import youtube_dl, cv2
+import os
 
 ydl_opts = {
 	'format': 'best',
@@ -7,6 +8,22 @@ ydl_opts = {
 ydl = youtube_dl.YoutubeDL(ydl_opts)
 
 test_id = 'KUfxaCyH2v0'
+curdir = os.getcwd()
+dldir = 'dl/'
 
-def youtubeDownload(video_id):
+def youtube_download(video_id):
+	os.chdir(dldir)
 	ydl.download(['https://www.youtube.com/watch?v=' + video_id])
+	os.chdir(curdir)
+
+'''
+Returns a cv2 VideoCapture object,
+or None if the file doesn't exist.
+'''
+def get_youtube_video(video_id):
+	for f in os.listdir(dldir):
+		if video_id in f:
+			return cv2.VideoCapture(dldir + f)
+
+
+
