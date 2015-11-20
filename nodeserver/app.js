f965d657dffdb0e4380289861d8a511729ad12c4
joker23 <zhangksteven@gmail.com>
2015-11-20 09:48:09 -0500

    modified return values for get function

--- nodeserver/app.js
+++ nodeserver/app.js
@@ -1,27 +1,28 @@
 var express = require('express');
 var app = express();
-
-rser = require('body-parser');
+var bodyParser = require('body-parser'); 
 
 app.get('/', function(req, res) {
 	res.send('hello world');
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+/* GET home page. */
 app.get('/', function(req, res, next) {
 
 	res.send({"names" : web3.eth.accounts});
 });
 
-app.post('/transaction', function(req, res) {
-	var video = req.body.huge_string;
-	console.log(web3.eth.accounts[0]);
+app.post('/putVideo', function(req, res) {
+	var video = req.body.videoHashes;
+	var url = req.body.url;
+	var redirect = req.body.redirect;
 
 	var ret = {};
 	var hash = web3.eth.sendTransaction({
@@ -32,7 +33,8 @@ app.post('/transaction', function(req, res) {
 		if (error) {
 			console.log(error);
 		} else {
-			if (putString(address, video)) {
+			if (putVideo(address, video) && putUrl(address, url)
+					&& putRedirect(address, redirect)) {
 				res.send({"addr": address});
 			} else {
 				res.send({"status":"failed"});
@@ -41,25 +43,49 @@ app.post('/transaction', function(req, res) {
 	});
 });
 
-app.get('/gethashes', function(req, res){
+app.get('/getAllData', function(req, res){
 	var trans_list = web3.eth.getBlock('pending').transactions;
 	var ret = [];
 
 	for (var i = 0; i<trans_list.length; i++) {
 		var h = trans_list[i];
-		ret.push(getString(h));
+		var ob = getUrl(h);
+		var rb = getVideo(h);
+		var sb = getRedirect(h);
+		var tm = {
+			redirect: sb,
+			videoHash: rb,
+			srcurl: ob
+		};
+		ret.push(tm);
 	}
 
-	res.send({"hash_list": ret});
+	res.send({"data": ret});
 });
 
 
-function putString(hash, str) {
+function putVideo(hash, str) {
 	return web3.db.putString('test', hash, str);
 }
 
-function getString(hash) {
+function putUrl(hash, str) {
+	return web3.db.putString('url', hash, str);
+}
+
+function putRedirect(hash, str) {
+	return web3.db.putString('redirect', hash, str);
+}
+
+function getVideo(hash) {
 	return web3.db.getString('test', hash);
+}
+
+function getUrl(hash) {
+	return web3.db.getString('url', hash);
+}
+
+function getRedirect(hash) {
+	return web3.db.getString('redirect', hash);
 }
 
 var server = app.listen(3000,"0.0.0.0", function () {
@@ -68,3 +94,4 @@ var server = app.listen(3000,"0.0.0.0", function () {
 
 	console.log('Example app listening at http://%s:%s', host, port);
 });
+
