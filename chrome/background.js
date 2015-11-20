94a4bb2ff29a723c91a6f4d5c8feeb97c3588065
William Yang <yangzwilliam@gmail.com>
2015-11-19 20:41:01 -0500

    can recognize facebook or youtube video pages and do stuff

--- chrome/background.js
+++ chrome/background.js
@@ -4,7 +4,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 		chrome.tabs.getSelected(null, function(tab) {
 		url = tab.url;
 		console.log(url);
-		if(url.indexOf("redirect2.html") < 0){
+		// if(url.indexOf("redirect2.html") < 0){
+		// 	window.open("redirect2.html");
+		// }
+		var hasFacebookVideo = url.indexOf("facebook") >= 0 && url.indexOf("video") >= 0;
+		var hasYoutubeVideo = url.indexOf("youtube") >= 0 && url.indexOf("watch?") >= 0;
+		if(hasFacebookVideo || hasYoutubeVideo){
 			window.open("redirect2.html");
 		}
 	});
